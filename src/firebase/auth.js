4d1c8a8e0989f5e14846b79e0ff839540e835cca
i-10101000 <ilya.krasnoper@gmail.com>
2023-12-26 19:54:57 +0200

    sdf

---
+++ src/firebase/auth.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwU2QEVvt1EQaynJkmWHWnwgZjoPrOqBo",
+  authDomain: "chatbook-1a50e.firebaseapp.com",
+  databaseURL: "https://chatbook-1a50e-default-rtdb.firebaseio.com",
+  projectId: "chatbook-1a50e",
+  storageBucket: "chatbook-1a50e.appspot.com",
+  messagingSenderId: "703517062082",
+  appId: "1:703517062082:web:9583bd124337a503411c5b",
+};
+
+
+export {authInitialize}
